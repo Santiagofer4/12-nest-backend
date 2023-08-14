5fcf8d16eefe19d2a7297dde6e3546adb32c620e
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-08-13 21:50:07 -0300

    section 342 finished

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,8 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
 
+import * as bcryptjs from 'bcryptjs'
+
 @Injectable()
 export class AuthService {
 
@@ -17,14 +19,24 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     try {
-      const newUser = new this.userModel(createUserDto);
-      return await newUser.save();
+      const { password, ...userData} = createUserDto
+      
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync( password, 10 ),
+        ...userData,
+      });
+      await newUser.save();
+      const { password:_, ...user } = newUser.toJSON()
+
+      return user
 
     } catch (error) {
+
       if ( error.code === 11000 ) {
         throw new BadRequestException(`${ createUserDto.email } already exist!`)
       }
       throw new InternalServerErrorException(`Something terrible happen!!!`)
+    
     }    
 
   }
