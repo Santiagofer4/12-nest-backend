80f49ab86fed7f26bd83e7643fa37fe41262a1d0
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-08-14 22:51:49 -0300

    section 344 finished

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,12 +8,16 @@ import { Model } from 'mongoose';
 
 import * as bcryptjs from 'bcryptjs'
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
 
   constructor(
-  @InjectModel( User.name ) private userModel: Model<User>
+  @InjectModel( User.name ) 
+  private userModel: Model<User>,
+  private jwtService: JwtService
   ) {
     
   }
@@ -59,7 +63,7 @@ export class AuthService {
 
     return {
       user: rest,
-      token: 'ABC-123'
+      token:  this.getJwtToken({ id: user.id }),
     }
   
   }
@@ -78,5 +82,11 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  getJwtToken( payload: JwtPayload ) {
+    const token = this.jwtService.sign(payload)
+
+    return token
   }
 }
