0bc7d3ba156a99e9ce571c9591dcd72bb1e59b53
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-08-15 22:50:52 -0300

    section 346 finished

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,7 @@
 import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { RegisterUserDto, UpdateAuthDto, CreateUserDto } from './dto';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
 
@@ -11,6 +10,7 @@ import { LoginDto } from './dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './interfaces/jwt-payload';
 import { LoginResponse } from './interfaces/login-response';
+
 
 @Injectable()
 export class AuthService {
@@ -46,10 +46,17 @@ export class AuthService {
     }    
   }
 
-  // async register(): Promise<LoginResponse> {
+  async register( registerDto: RegisterUserDto): Promise<LoginResponse> {
+    
+    const user = await this.create( registerDto )
+
+    return {
+      user: user,
+      token: this.getJwtToken( {id: user._id } )
+    }
     
 
-  // }
+  }
 
   async login( loginDto: LoginDto ): Promise<LoginResponse> {
 
